508ee08482fce0b77580b8511b277d910637c681
MatheusVMariussi <168836383+MatheusVMariussi@users.noreply.github.com>
2024-09-30 12:00:54 -0300

    Add files via upload

---
+++ AtividadesAulas/3009/exercicio15/exercicio15.py
@@ -0,0 +1,18 @@
+#exercicio15.py
+from flask import Flask, render_template, request
+from login import login_bp
+from sensors import sensors_bp
+from actuators import actuators_bp
+
+app = Flask(__name__)
+
+app.register_blueprint(login_bp, url_prefix='/')
+app.register_blueprint(sensors_bp, url_prefix='/')
+app.register_blueprint(actuators_bp, url_prefix='/')
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ AtividadesAulas/3009/exercicio15/sensors.py
@@ -0,0 +1,45 @@
+#sensors.py
+from flask import Blueprint, request, render_template, redirect, url_for
+
+sensors_bp = Blueprint("sensors",__name__, template_folder="templates")
+
+sensors = {}
+
+@sensors_bp.route('/sensors')
+def sensors_view():
+    return render_template("sensors.html", sensors=sensors)
+
+@sensors_bp.route('/register_sensor')
+def reg_sensor():
+    return render_template("register_sensor.html")
+
+@sensors_bp.route('/add_sensor', methods=['GET', 'POST'])
+def add_sensor():
+    if request.method == 'POST':
+        key = request.form['key']
+        value = request.form['value']
+    else:
+        key = request.args.get('key', None)
+        value = request.args.get('value', None)
+    
+    if key and value:
+        sensors[key] = value
+    return render_template("sensors.html", sensors=sensors)
+
+@sensors_bp.route('/remove_sensor')
+def remove_sensor():
+    return render_template("remove_sensor.html", sensors=sensors)
+                           
+@sensors_bp.route('/del_sensor', methods=['GET','POST'])
+def del_sensor():
+    if request.method == 'POST':
+        key = request.form['key']
+        value = request.form['value']
+    else:
+        key = request.args.get('key', None)
+        value = request.args.get('value', None)
+
+    if key and value:
+        sensors.pop(key)
+    
+    return render_template("sensors.html", sensors=sensors)

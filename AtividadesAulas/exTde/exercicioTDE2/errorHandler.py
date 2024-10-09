702fbbd263c9576fb93d25928e1cd663dc0b5ba2
MatheusVMariussi <168836383+MatheusVMariussi@users.noreply.github.com>
2024-10-09 19:50:39 -0300

    Add files via upload

---
+++ AtividadesAulas/exTde/exercicioTDE2/errorHandler.py
@@ -0,0 +1,36 @@
+# errorHandler.py
+from flask import Blueprint, request, render_template, redirect, url_for
+
+error = Blueprint("error",__name__, template_folder="templates")
+
+@error.errorhandler(401)
+def page_not_found(error):
+    return render_template('401.html'), 401
+
+@error.errorhandler(403)
+def page_not_found(error):
+    return render_template('403.html'), 403
+
+@error.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@error.errorhandler(405)
+def page_not_found(error):
+    return render_template('405.html'), 405
+
+@error.errorhandler(408)
+def page_not_found(error):
+    return render_template('408.html'), 408
+
+@error.errorhandler(429)
+def page_not_found(error):
+    return render_template('429.html'), 429
+
+@error.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
+
+@error.errorhandler(503)
+def page_not_found(error):
+    return render_template('503.html'), 503
